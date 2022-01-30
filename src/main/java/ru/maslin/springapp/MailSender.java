6cd8a50c3acefbd7177a05cec878088f3e72f9f6
Nikita Maslin <NMaslin@bellintegrator.com>
2022-01-30 21:37:47 +0400

    mail sendgrid

--- src/main/java/ru/maslin/springapp/MailSender.java
+++ src/main/java/ru/maslin/springapp/MailSender.java
@@ -1,32 +1,40 @@
 package ru.maslin.springapp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
-@Service
+import java.io.IOException;
+
+@Component
+@Slf4j
 public class MailSender {
 
-    private final JavaMailSender javaMailSender;
-
-    @Value("${spring.mail.username}")
-    private String username;
-
-    @Autowired
-    public MailSender(JavaMailSender javaMailSender) {
-        this.javaMailSender = javaMailSender;
-    }
-
     public void send(String emailTo, String subject, String message) {
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        Email from = new Email("cpkia-message@yandex.ru");
+        Email to = new Email(emailTo);
+        Content content = new Content("text/plain", message);
+        Mail mail = new Mail(from, subject, to, content);
 
-        simpleMailMessage.setFrom(username);
-        simpleMailMessage.setTo(emailTo);
-        simpleMailMessage.setSubject(subject);
-        simpleMailMessage.setText(message);
-
-        javaMailSender.send(simpleMailMessage);
+        SendGrid sg = new SendGrid("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");
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            log.error("Error send mail", ex);
+        }
     }
 }
+
