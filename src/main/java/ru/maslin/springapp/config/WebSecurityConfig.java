9f2212b609026da256e938ecd413361e8b348526
Nikita Maslin <NMaslin@bellintegrator.com>
2022-01-31 12:46:07 +0400

    resources css

--- src/main/java/ru/maslin/springapp/config/WebSecurityConfig.java
+++ src/main/java/ru/maslin/springapp/config/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/company/**", "/resources/**", "/css/**")
+                .antMatchers("/", "/company/**", "/resources/**", "/static/css/**")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -44,7 +44,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) {
         web.ignoring()
                 .antMatchers(
-                        "/css/**", "/fonts/**",
+                        "/static/css/**", "/fonts/**",
                         "/images/**");
     }
 
